cb73a6b72dce3b4090cc7f1c8c4d35a7d946ebd9
phsoo <91250611+smwater@users.noreply.github.com>
2022-08-10 17:29:13 +0900

    220810

---
+++ 220810/220810/466p 예제.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace UsingCallback
+{
+    delegate int Compare<T>(T a, T b);
+
+    class MainApp
+    {
+        static int AscendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+
+        static int DescendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) * -1;
+        }
+
+        static void BubbleSort<T>(T[] DataSet, Compare<T> Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            T temp;
+
+            for (i = 0; i < DataSet.Length - 1; i++)
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort<int>(array, new Compare<int>(AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort<int>(array2, new Compare<int>(DescendCompare));
+
+            for (int i = 0; i < array2.Length; i++)
+                Console.Write($"{array2[i]} ");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 220810/220810/Program.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+
+//namespace Delegate
+//{
+//    delegate int MyDelegate( int a, int b );
+
+//    class Calculator
+//    {
+//        public int Plus(int a, int b)
+//        {
+//            return a + b;
+//        }
+
+//        public static int Minus(int a, int b)
+//        {
+//            return a - b;
+//        }
+//    }
+
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Calculator Calc = new Calculator();
+//            MyDelegate CallBack;
+
+//            CallBack = new MyDelegate(Calc.Plus);
+//            Console.WriteLine(CallBack(3, 4));
+
+//            CallBack = new MyDelegate(Calculator.Minus);
+//            Console.WriteLine(CallBack(7, 5));
+//        }
+//    }
+//}
